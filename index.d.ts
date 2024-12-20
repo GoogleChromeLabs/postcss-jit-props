dd53dd0c11a8406d61b1ebc25d9a8e51c6675225
tanishqmanuja <tanishqmanuja@gmail.com>
2024-12-20 12:13:32 +0530

    feat: add types

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+import { PluginCreator } from "postcss";
+
+export interface Options {
+  /**
+   * Wraps the entire rule inside `@layer` syntax.
+   */
+  layer?: string;
+
+  /**
+   * Where search for css props, globbing allowed.
+   */
+  files?: string[];
+
+  /**
+   * Selector where the props are pushed to instead of `:root`.
+   */
+  custom_selector?: string;
+}
+
+type Props = Record<string, string | number>;
+
+export type PostcssJitPropsOptions = Options & Props;
+declare const postcssJitProps: PluginCreator<PostcssJitPropsOptions>;
+
+export default postcssJitProps;
